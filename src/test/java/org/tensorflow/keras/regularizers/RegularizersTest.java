7969be3b0ae5e834a9c00eebb0924b93e11a6659
Jim Clarke <JimClarke5@me.com>
2020-07-21 18:39:21 -0400

    fix imports

--- src/test/java/org/tensorflow/keras/regularizers/RegularizersTest.java
+++ src/test/java/org/tensorflow/keras/regularizers/RegularizersTest.java
@@ -17,7 +17,6 @@ package org.tensorflow.keras.regularizers;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
